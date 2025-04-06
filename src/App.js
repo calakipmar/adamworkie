05f6c095de11236a03d1e6737c74beee9e9e3df1
calakipmar <106762081+calakipmar@users.noreply.github.com>
2025-04-05 18:44:09 -0600

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>TECH2102 - Enterprise Computing - 25JANMNFS2</h2>
         <Name/>
       </header>
     </div>
