d5016eea0361f24fcb38e6bc3d290a1e6f8894e5
calakipmar <106762081+calakipmar@users.noreply.github.com>
2025-04-05 18:54:57 -0600

    Setting autodeploy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>TECH2102 - Enterprise Computing - 25JANMNFS2</h2>
+        {/* <h2>TECH2102 - Enterprise Computing - 25JANMNFS2</h2> */}
         <Name/>
       </header>
     </div>
