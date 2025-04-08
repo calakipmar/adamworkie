e3532e94ccd1f1fb61bb9b82a1cdb967f55ac20d
calakipmar <106762081+calakipmar@users.noreply.github.com>
2025-04-08 13:03:41 -0600

    Modified App.js for inclass assessment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>TECH2102 - Enterprise Computing - 25JANMNFS2</h2>
         <Name/>
       </header>
     </div>
